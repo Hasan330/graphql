790ebddaaa5edb0bbe808060e54e9002cba4bae8
Hasan Khader <hasan.saleh.khader@gmail.com>
2019-02-07 16:59:09 +0100

    Initial steps in adding prisma

--- src/index.js
+++ src/index.js
@@ -1,49 +1,20 @@
 const { GraphQLServer } = require('graphql-yoga')
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews Clone`,
-        feed: () => links,
-        link: (root, args) => {
-            console.log(`Searching for link with id: ${args.id}`)
-            return links.find(item => item.id == args.id)}
+        feed: (root, args, context, info) => {
+            //It accesses a prisma object on context
+            return context.prisma.links()
+        },
     },
     Mutation: {
-        post: (root, args) => {
-            const link = {
-                id: `link-${idCount++}`,
-                description: args.description,
+        post: (root, args, context) => {
+            return context.prisma.createLink({
                 url: args.url,
-            }
-            links.push(link)
-            return link
+                description: args.description,
+            })
         },
-        
-        updateLink: (root, args) => {
-            links.forEach((element, index, arr) => {
-               if(element.id === args.id) arr[index] = args;
-            });
-    
-            return args;
-        },
-
-        deleteLink: (root, args) => {
-            let deletedItem;
-            links.forEach((element, index, arr) => {
-                if (element.id === args.id){
-                    deletedItem = arr.pop(index);
-                } 
-            });
-            
-            return deletedItem;
-        }
     },
 }
 
